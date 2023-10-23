be1f60cfb8a6ef4db4ab6456065b631a471a357b
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-10-24 08:49:12 +0900

    실전예제4 - 상속관계매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop;
 
 
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -18,13 +20,10 @@ public class JpaMain {
         tx.begin();     //트랜잭션 시작
         //code
         try {
-            Order order = new Order();
-            em.persist(order);
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
 
             tx.commit();    //커밋
         } catch (Exception e) {
