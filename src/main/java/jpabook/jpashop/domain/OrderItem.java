d7f29a79c1387ff6e1dbb828f8794d385e66135a
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-10-31 08:28:08 +0900

    실전예제5 - 연관관계 관리

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -7,11 +7,11 @@ public class OrderItem extends BaseEntity  {
     @Id @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
     private int orderPrice;
